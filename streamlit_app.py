0a9912549bd2ccd7d151eab2b4b956d43558fdc9
Xinyu Wu <xy.wu91@gmail.com>
2023-10-06 10:23:33 -0400

    Prompt to make sure the chatbot speak in the interest of Pietra

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from llama_index import VectorStoreIndex, ServiceContext, StorageContext
+from llama_index import VectorStoreIndex, ServiceContext, StorageContext, PromptTemplate
+from llama_index.chat_engine import CondenseQuestionChatEngine, ContextChatEngine
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader, load_index_from_storage
+from llama_index.memory import ChatMemoryBuffer
 
 st.set_page_config(page_title="Chat with the Pietra's FAQ docs, powered by LlamaIndex", page_icon="🦙", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
@@ -25,7 +27,7 @@ def load_data():
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5,
-                                                                  system_prompt="You are an expert on the Pietra FAQ and your job is to answer questions. Assume that all questions are related to the Pietra. Keep your answers based on facts – do not hallucinate features."))
+                                                                  system_prompt="You are an expert on the Pietra FAQ and your job is to answer questions. Assume that all questions are related to the Pietra. You should always speak in the interest of Pietra. Keep your answers based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
 
 
@@ -42,7 +44,38 @@ index = load_data()
 # chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
 
 if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode="context", verbose=True)
+    custom_prompt = PromptTemplate("""\
+    You represent Pietra to answer customer's questions. Your answer should always favor Pietra.
+
+    <Chat History>
+    {chat_history}
+
+    <Follow Up Message>
+    {question}
+
+    <Standalone question>
+    """)
+
+    query_engine = index.as_query_engine()
+    service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0613", temperature=1,
+                                                              system_prompt="You are an expert on the Pietra FAQ and your job is to answer questions. Assume that all questions are related to the Pietra. You should always favor Pietra in the answer."))
+
+    # chat_engine = ContextChatEngine.from_defaults(
+    #     retriever=index.as_retriever(),
+    #     query_engine=query_engine,
+    #     condense_question_prompt=custom_prompt,
+    #     service_context=service_context,
+    #     verbose=True
+    # )
+
+    memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
+    chat_engine = index.as_chat_engine(
+        chat_mode="context",
+        memory=memory,
+        system_prompt="You are an expert on the Pietra FAQ and your job is to answer questions. Assume that all questions are related to the Pietra. You should always favor Pietra in the answer.",
+    )
+
+    st.session_state.chat_engine = chat_engine
 
 if prompt := st.chat_input("Your question"):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
